23313a8c81a4be893c61a16b0543f3638bd79e75
Michael Gackle <michael.gackle@ad.aie.edu>
2016-11-15 13:52:02 +1030

    Options started

---
+++ AGS - Endless/Assets/progressionScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor.SceneManagement;
+
+public class progressionScript : MonoBehaviour {
+
+    public bool isCareerMode = false;
+    int currentLevel;
+    int score;
+    public string name = "";
+
+    bool progress = false;
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+    void progression(bool levelchange, GameObject other)
+    { if (isCareerMode) { currentLevel++; } progress = true; }
+	// Update is called once per frame
+	void Update ()
+    {
+	
+        if(progress && isCareerMode)
+        {
+            progress = false;
+        }
+	}
+}
